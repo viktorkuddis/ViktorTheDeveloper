2f4a3c760e801d3d22df843ea55f276886ee6b26
Viktor Magnusson <viktor.kuddis@hotmail.com>
2026-02-25 11:03:20 +0100

    adds navbar layout

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,8 +1,22 @@
+import styles from './NavBar.module.css'
+
 export default function NavBar() {
     return (
-        <div className="content-container-width content-container-padding">
-            <p>DETTA ÄR NAVBAR</p>
+
+        <div className={`${styles.container} content-container-width content-container-padding`}>
+            <p className={`headding-loud ${styles.logo}`}>VIKTOR.</p>
+            <nav>
+                <ul>
+                    <li><a href="#">Hem</a></li>
+                    <li><a href="#">Om</a></li>
+                    <li><a href="#">Erfarenhet</a></li>
+                    <li><a href="#">Projekt</a></li>
+                    <li> <a href="#">Kontakt</a></li>
+                </ul>
+            </nav>
         </div>
+
+
 
     )
 }

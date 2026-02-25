518efc913813d8db58194d14e0e2ebd6206f96a1
Viktor Magnusson <viktor.kuddis@hotmail.com>
2026-02-25 18:22:40 +0100

    adds layout to hero section

--- src/Components/HeroSection/HeroSection.tsx
+++ src/Components/HeroSection/HeroSection.tsx
@@ -1,10 +1,36 @@
 
+import ImageContainer from '../ImageContainer/ImageContainer'
+import styles from './HeroSection.module.css'
+
+import profilePic from "@/assets/ViktorMagnussonCompressed.jpg";
+
+
 
 export default function HeroSection() {
     return (
-        <div className="content-container-width content-container-padding">HeroSection
+        <div className={`${styles.wrapper}`}>
+            <div className={`${styles.contentContainer} content-container-width content-container-padding`}>
 
-            <h1 className="headding-loud">HEJ JAG HETER VIKTOR</h1>
+
+                <div className={`${styles.imageArea}`}>
+                    <ImageContainer src={profilePic} alt={''} borderRadius={"medium"} aspectRatio='1/1' />
+                </div>
+
+
+
+                <div className={`${styles.textArea}`}>
+                    <h1 className="headding-loud">
+                        JAG ÄR {" "}
+                        {/* break on big screan är en radbrytning som bara används  när vi går till stor skärm  */}
+                        <br className={`${styles.breakOnBigScreen}`} />
+                        UTVECKLARE
+                    </h1>
+                    <p> Som nyutbildad utvecklare med fokus på frontend ser jag nu fram emot att äntligen börja jobba och utveckla(s) tillsammans med er.</p>
+
+                    <button>Mer om mig</button>
+                </div>
+            </div>
         </div>
+
     )
 }
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -2,18 +2,22 @@ import styles from './NavBar.module.css'
 
 export default function NavBar() {
     return (
+        <div className={`${styles.wrapper}`}>
 
-        <div className={`${styles.container} content-container-width content-container-padding`}>
-            <p className={`headding-loud ${styles.logo}`}>VIKTOR.</p>
-            <nav>
-                <ul>
-                    <li><a href="#">Hem</a></li>
-                    <li><a href="#">Om</a></li>
-                    <li><a href="#">Erfarenhet</a></li>
-                    <li><a href="#">Projekt</a></li>
-                    <li> <a href="#">Kontakt</a></li>
-                </ul>
-            </nav>
+
+            <div className={`${styles.container} content-container-width content-container-padding`}>
+                <p className={`headding-loud ${styles.logo}`}>VIKTOR.</p>
+
+                <nav>
+                    <ul>
+                        <li><a href="#">Hem</a></li>
+                        <li><a href="#">Om</a></li>
+                        <li><a href="#">Erfarenhet</a></li>
+                        <li><a href="#">Projekt</a></li>
+                        <li> <a href="#">Kontakt</a></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
 
 

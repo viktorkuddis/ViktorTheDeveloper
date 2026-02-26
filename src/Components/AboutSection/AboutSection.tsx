22171633b9b7d482069bf2a4d8c1cf26b8baf0b0
Viktor Magnusson <viktor.kuddis@hotmail.com>
2026-02-26 18:33:30 +0100

    about text is layouted

--- src/Components/AboutSection/AboutSection.tsx
+++ src/Components/AboutSection/AboutSection.tsx
@@ -1,9 +1,18 @@
+import aboutText from "@/data/aboutText.json"
+
+import styles from "./AboutSection.module.css"
+
 
 export default function AboutSection() {
     return (
-        <div className="content-container-width content-container-padding">
-            <div>AboutSection</div>
-        </div>
+        <div className={`${styles.contentContainer} content-container-width content-container-padding`} >
+            <h2 className={`headding-loud`}>OM MIG</h2>
+
+            <div className={`${styles.textContainer} `}>
+                <p>{aboutText.text} </p>
+
+            </div>
+        </div >
 
     )
 }

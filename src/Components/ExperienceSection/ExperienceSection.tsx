83f111aa1f5fde6daa1b91cefca2f0ffb2f52f62
Viktor Magnusson <viktor.kuddis@hotmail.com>
2026-03-07 16:05:27 +0100

    margins top to bottom refactor for eye-measurements

--- src/Components/ExperienceSection/ExperienceSection.tsx
+++ src/Components/ExperienceSection/ExperienceSection.tsx
@@ -5,15 +5,16 @@ export default function ExperienceSection() {
 
     return (<section className={`${styles.experienceSection}`}>
         <div className="content-container-width content-container-padding">
-
-            Erfarenhet
+            <h2 className={`headding-loud-inverted ${styles.heading}`}>
+                ERFARENHET
+            </h2>
 
 
             Här byggs på med komponenter för varje erfarenhet.
             Den är blå om den är current true.
             För enlast sortering = Det staplas i den ordning den ligger i arrayen. Inte enligt tid.
         </div >
-    </section>
+    </section >
 
 
     )

f3ea61aec014a12934ba6b00225058876b077348
Viktor Magnusson <viktor.kuddis@hotmail.com>
2026-03-07 15:43:31 +0100

    base setup for section

--- src/Components/ExperienceSection/ExperienceSection.tsx
+++ src/Components/ExperienceSection/ExperienceSection.tsx
@@ -1,8 +1,20 @@
 
+import styles from "./ExperiencseSection.module.css"
+
 export default function ExperienceSection() {
-    return (<div className="content-container-width content-container-padding">
-        <div>ExperienceSection</div>
-    </div>
+
+    return (<section className={`${styles.experienceSection}`}>
+        <div className="content-container-width content-container-padding">
+
+            Erfarenhet
+
+
+            Här byggs på med komponenter för varje erfarenhet.
+            Den är blå om den är current true.
+            För enlast sortering = Det staplas i den ordning den ligger i arrayen. Inte enligt tid.
+        </div >
+    </section>
+
 
     )
 }

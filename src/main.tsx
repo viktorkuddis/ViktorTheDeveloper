a141bb81e29409c4e8a31af3739022f32c7d2ffd
Viktor Magnusson <viktor.kuddis@hotmail.com>
2026-03-11 18:26:04 +0100

    typo

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './indexBaseUi.css'
+import './indexBaseUI.css'
 import './index.css'
 
 import App from './App.tsx'

06a4492ff7f051ef445875c04e2b44d6e304f5fc
Viktor Magnusson <viktor.kuddis@hotmail.com>
2026-03-11 11:45:01 +0100

    clean up whitespace in ProjectCard component

--- src/Components/ProjectCard/ProjectCard.tsx
+++ src/Components/ProjectCard/ProjectCard.tsx
@@ -13,51 +13,38 @@ export default function ProjectCard({ project }: Props) {
     return (
         <div className={`${styles.projectCardCONTAINER}`}>
 
-
             <div role="button" className={`${styles.projectCard}`}>
 
-
-
-
-
-
                 <div className={`${styles.informationContainer}`}>
-
-
                     <div>
-
                         <h3 className={`${styles.heading}`}>{project.title}</h3>
-
                     </div>
-
-
                     <div>
-                        <p className={`${styles.textChunk}`}>{project.summary}</p>
+                        <p className={`${styles.textChunk}`}>
+                            {project.summary}
+                        </p>
+                        {/* taggar: */}
                         {(project.projectContext || project.projectForm) && (
                             <>
-
-
                                 {project.projectContext && <span className={`${styles.tag}`}>{project.projectContext}</span>}
                                 {project.projectForm && <span className={`${styles.tag}`}>{project.projectForm}</span>}
                                 {project.label &&
                                     project.label.map((l, i) => <span key={i} className={`${styles.tag}`}> {l}</span>)}
-
                             </>
                         )}
                     </div>
-
-
                     <div className={`${styles.callToAction}`}>
-                        <span>Se mer</span> <ArrowRight className={`${styles.arrow}`} /></div>
+                        <span>Se mer</span> <ArrowRight className={`${styles.arrow}`} />
+                    </div>
                 </div>
+
                 {project.coverImage &&
                     <div className={`${styles.coverImage}`}>
                         <ImageContainer src={project.coverImage} alt={""} width="100%" borderRadius={"small"} height="100%" />
-
                     </div>}
 
-
             </div>
+
         </div >
     )
 }

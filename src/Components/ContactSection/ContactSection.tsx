a4915c96158c292b526a6e1f7f2128eaec9ba1f2
Viktor Magnusson <viktor.kuddis@hotmail.com>
2026-03-11 15:25:02 +0100

    implement Base UI integration: add root styles

--- src/Components/ContactSection/ContactSection.tsx
+++ src/Components/ContactSection/ContactSection.tsx
@@ -1,10 +1,11 @@
+// import styles from "./ContactSection.module.css"
+
 
 export default function ContactSection() {
     return (
 
         <div className="content-container-width content-container-padding">
             <div>ContactSection</div>
-
         </div>
     )
 }
